ba6072cc786677f79a2b4d088a4fbe2077c45310
Vivek <tailorvivekgec19@gmail.com>
2023-02-20 23:05:05 -0800

    EMI Calculat

--- lib/Screen View/screen_view.dart
+++ lib/Screen View/screen_view.dart
@@ -6,8 +6,10 @@ class Screen_View extends StatefulWidget {
   @override
   State<Screen_View> createState() => _Screen_ViewState();
 }
-
+double loan_amount = 5000;
 double emi = 0;
+double rate = 1 ;
+double tenure = 1 ;
 
 class _Screen_ViewState extends State<Screen_View> {
   @override
@@ -42,7 +44,7 @@ class _Screen_ViewState extends State<Screen_View> {
                         child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
                             Text("EMI",style: TextStyle(fontSize: 20,color: Colors.white),),
-                            Text("\$ ${emi}",style: TextStyle(fontSize: 20,color: Colors.white),),
+                            Text("\$ ${emi.toInt()}",style: TextStyle(fontSize: 20,color: Colors.white),),
                           ],
                         ),
                       ),
@@ -61,19 +63,19 @@ class _Screen_ViewState extends State<Screen_View> {
                             Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
                               children: [
                                 Text("Loan Amount",style: TextStyle(fontSize: 20,color: Colors.black),),
-                                Text("\$ ${emi}",style: TextStyle(fontSize: 20,color: Colors.black),),
+                                Text("\$ ${loan_amount.toInt()}",style: TextStyle(fontSize: 20,color: Colors.black),),
                               ],
                             ),
                             Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
                               children: [
                                 Text("Interest Rate",style: TextStyle(fontSize: 20,color: Colors.black),),
-                                Text("${emi} %",style: TextStyle(fontSize: 20,color:Colors.black),),
+                                Text("${rate.toInt()} %",style: TextStyle(fontSize: 20,color:Colors.black),),
                               ],
                             ),
                             Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
                               children: [
                                 Text("Tenure",style: TextStyle(fontSize: 20,color: Colors.black),),
-                                Text("${emi} Yr",style: TextStyle(fontSize: 20,color: Colors.black),),
+                                Text("${tenure.toInt()} Yr",style: TextStyle(fontSize: 20,color: Colors.black),),
                               ],
                             ),
                           ],
@@ -95,8 +97,101 @@ class _Screen_ViewState extends State<Screen_View> {
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.all(Radius.circular(25)),color: Colors.black12),
                 child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-
+                    SizedBox(height: 10,),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "Enter Loan Amount",
+                            style: TextStyle(color: Colors.black, fontSize: 20),
+                          ),
+                          Text(
+                            "\$  ${loan_amount.toInt()}",
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 20,
+                                ),
+                          )
+                        ],
+                      ),
+                    ),
+                    Slider(
+                        value: loan_amount,
+                        divisions: 10,
+                        min: 5000,
+                        max: 10000,
+                        onChanged: (value) {
+                          setState(() {
+                            loan_amount = value;
+                            emi = loan_amount * rate * tenure /100;
+                          });
+                        }),
+                    SizedBox(height: 10,),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "Enter Interest Rate p.a",
+                            style: TextStyle(color: Colors.black, fontSize: 20),
+                          ),
+                          Text(
+                            "${rate.toInt()} %",
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 20,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    Slider(
+                        value: rate,
+                        divisions: 30,
+                        max: 30,
+                        min: 1,
+                        onChanged: (value) {
+                          setState(() {
+                            rate = value;
+                            emi = loan_amount * rate * tenure /100;
+                          });
+                        }),
+                    SizedBox(height: 10,),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "Enter Tenure",
+                            style: TextStyle(color: Colors.black, fontSize: 20),
+                          ),
+                          Text(
+                            "${tenure.toInt()} Yr",
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 20,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    Slider(
+                        value: tenure,
+                        divisions: 20,
+                        max: 20,
+                        min: 1,
+                        onChanged: (value) {
+                          setState(() {
+                            tenure = value;
+                            emi = loan_amount * rate * tenure /100;
+                          });
+                        }),
                   ],
                 ),
               ),
